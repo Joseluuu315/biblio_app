b6f56d41043ee78428ee2c4d86d9999e1ff1c9dd
José Luis Fuentes Parra <josefnts3115@gmail.com>
2025-11-19 09:38:09 +0100

    Initial commit

---
+++ src/test/java/com/joseluu/biblio_app/BiblioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluu.biblio_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
